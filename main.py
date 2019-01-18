d86ae8c09b0ed3c0f03ab3d5a54207963870971f
Susmit-A <susmit600@gmail.com>
2019-01-18 18:14:19 +0530

    Update

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/profile")
 def profile():
-	return render_template("profile.html")
+    return render_template("profile.html")
 
 
 @app.route("/department")
 def department():
-	return render_template("department.html", departments=Department.fetch_all())
+    return render_template("department.html", departments=Department.fetch_all())
 
 
 @app.route("/departmentinfo", methods=['GET', 'POST'])
 def departmentinfo():
-	dept = request.args['department']
-	dept_name = Department.get_name(dept)
-	staff = list(dict(Teacher.fetch_all(dept)).values())
-	students = dict(Student.fetch_all(dept=dept, sem=5))
-	return render_template("department_info.html", dept=dept_name, staff=staff, students=students)
+    dept = request.args['department']
+    dept_name = Department.get_name(dept)
+    staff = list(dict(Teacher.fetch_all(dept)).values())
+    students = dict(Student.fetch_all(dept=dept, sem=5))
+    return render_template("department_info.html", dept=dept_name, staff=staff, students=students)
 
 
 FirebaseObject()
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
